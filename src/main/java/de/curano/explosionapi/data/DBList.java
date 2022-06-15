3f13e7ebc8a6fac731de675a9854972b9dd31099
TheCurano <thecurano@outlook.de>
2022-06-15 22:36:02 +0200

    Start of MySQL Systems

---
+++ src/main/java/de/curano/explosionapi/data/DBList.java
@@ -0,0 +1,271 @@
+package de.curano.explosionapi.data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class DBList {
+
+    private DBManager dbManager;
+    private String listName;
+
+    public DBList(DBManager dbManager, String listName) {
+        this.dbManager = dbManager;
+        this.listName = listName;
+        createIfNotExists();
+    }
+
+    /**
+     * Types: string 1 | int 2 | long 3 | double 4 | boolean 5 | float 6
+     */
+    private void createIfNotExists() {
+        dbManager.createTable(listName + "(name VARCHAR(256), type BIT, value VARCHAR(4096))");
+    }
+
+    public String getString(String name) {
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 1 AND name = '" + name + "';");
+        try {
+            if (result.next()) {
+                return result.getString(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Integer getInt(String name) {
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 2 AND name = '" + name + "';");
+        try {
+            if (result.next()) {
+                return Integer.parseInt(result.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Long getLong(String name) {
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 3 AND name = '" + name + "';");
+        try {
+            if (result.next()) {
+                return Long.parseLong(result.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Double getDouble(String name) {
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 4 AND name = '" + name + "';");
+        try {
+            if (result.next()) {
+                return Double.parseDouble(result.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Boolean getBoolean(String name) {
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 5 AND name = '" + name + "';");
+        try {
+            if (result.next()) {
+                return Boolean.parseBoolean(result.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Float getFloat(String name) {
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 6  AND name = '" + name + "';");
+        try {
+            if (result.next()) {
+                return Float.parseFloat(result.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getStringOrDefault(String name, String defaultValue) {
+        String value = getString(name);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public Integer getIntOrDefault(String name, Integer defaultValue) {
+        Integer value = getInt(name);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public Long getLongOrDefault(String name, Long defaultValue) {
+        Long value = getLong(name);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public Double getDoubleOrDefault(String name, Double defaultValue) {
+        Double value = getDouble(name);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public Boolean getBooleanOrDefault(String name, Boolean defaultValue) {
+        Boolean value = getBoolean(name);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public Float getFloatOrDefault(String name, Float defaultValue) {
+        Float value = getFloat(name);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    public void set(String name, String value) {
+        if (getString(name) == null) {
+            dbManager.getDatabase().execute("INSERT INTO " + dbManager.getDatabase().getDatabaseName() + "." + listName + " (name, type, value) VALUES ('" + name + "', 1, '" + value + "');");
+        } else {
+            dbManager.getDatabase().execute("UPDATE " + dbManager.getDatabase().getDatabaseName() + "." + listName + " SET value = '" + value + "' WHERE name = '" + name + "';");
+        }
+    }
+
+    public void set(String name, int value) {
+        if (getString(name) == null) {
+            dbManager.getDatabase().execute("INSERT INTO " + dbManager.getDatabase().getDatabaseName() + "." + listName + " (name, type, value) VALUES ('" + name + "', 2, '" + value + "');");
+        } else {
+            dbManager.getDatabase().execute("UPDATE " + dbManager.getDatabase().getDatabaseName() + "." + listName + " SET value = '" + value + "' WHERE name = '" + name + "';");
+        }
+    }
+
+    public void set(String name, long value) {
+        if (getString(name) == null) {
+            dbManager.getDatabase().execute("INSERT INTO '" + dbManager.getDatabase().getDatabaseName() + "." + listName + " (name, type, value) VALUES ('" + name + "', 3, '" + value + "');");
+        } else {
+            dbManager.getDatabase().execute("UPDATE " + dbManager.getDatabase().getDatabaseName() + "." + listName + " SET value = '" + value + "' WHERE name = '" + name + "';");
+        }
+    }
+
+    public void set(String name, double value) {
+        if (getString(name) == null) {
+            dbManager.getDatabase().execute("INSERT INTO " + dbManager.getDatabase().getDatabaseName() + "." + listName + " (name, type, value) VALUES ('" + name + "', 4, '" + value + "');");
+        } else {
+            dbManager.getDatabase().execute("UPDATE " + dbManager.getDatabase().getDatabaseName() + "." + listName + " SET value = '" + value + "' WHERE name = '" + name + "';");
+        }
+    }
+
+    public void set(String name, boolean value) {
+        if (getString(name) == null) {
+            dbManager.getDatabase().execute("INSERT INTO " + dbManager.getDatabase().getDatabaseName() + "." + listName + " (name, type, value) VALUES ('" + name + "', 5, '" + value + "');");
+        } else {
+            dbManager.getDatabase().execute("UPDATE " + dbManager.getDatabase().getDatabaseName() + "." + listName + " SET value = '" + value + "' WHERE name = '" + name + "';");
+        }
+    }
+
+    public void set(String name, float value) {
+        if (getString(name) == null) {
+            dbManager.getDatabase().execute("INSERT INTO " + dbManager.getDatabase().getDatabaseName() + "." + listName + " (name, type, value) VALUES ('" + name + "', 6, '" + value + "');");
+        } else {
+            dbManager.getDatabase().execute("UPDATE " + dbManager.getDatabase().getDatabaseName() + "." + listName + " SET value = '" + value + "' WHERE name = '" + name + "';");
+        }
+    }
+
+    public HashMap<String, String> getAllStrings() {
+        HashMap<String, String> map = new HashMap<>();
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT name, value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 1;");
+        try {
+            while (result.next()) {
+                map.put(result.getString(1), result.getString(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    public HashMap<String, Integer> getAllInts() {
+        HashMap<String, Integer> map = new HashMap<>();
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT name, value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 2;");
+        try {
+            while (result.next()) {
+                map.put(result.getString(1), Integer.parseInt(result.getString(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    public HashMap<String, Long> getAllLongs() {
+        HashMap<String, Long> map = new HashMap<>();
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT name, value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 3;");
+        try {
+            while (result.next()) {
+                map.put(result.getString(1), Long.parseLong(result.getString(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    public HashMap<String, Double> getAllDoubles() {
+        HashMap<String, Double> map = new HashMap<>();
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT name, value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 4;");
+        try {
+            while (result.next()) {
+                map.put(result.getString(1), Double.parseDouble(result.getString(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    public HashMap<String, Boolean> getAllBooleans() {
+        HashMap<String, Boolean> map = new HashMap<>();
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT name, value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 5;");
+        try {
+            while (result.next()) {
+                map.put(result.getString(1), Boolean.parseBoolean(result.getString(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    public HashMap<String, Float> getAllFloats() {
+        HashMap<String, Float> map = new HashMap<>();
+        ResultSet result = dbManager.getDatabase().executeQuery("SELECT name, value FROM " + dbManager.getDatabase().getDatabaseName() + "." + listName + " WHERE type = 6;");
+        try {
+            while (result.next()) {
+                map.put(result.getString(1), Float.parseFloat(result.getString(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+}
---
+++ src/main/java/de/curano/explosionapi/data/Database.java
@@ -0,0 +1,180 @@
+package de.curano.explosionapi.data;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class Database {
+
+    private DatabaseType type;
+    private String host;
+    private int port;
+    private String database;
+    private String username;
+    private String password;
+    private Connection connection = null;
+    private HikariDataSource dataSource = null;
+    private PoolSettings poolSettings;
+
+    public Database(DatabaseType type, String host, int port, String database, String username, String password) {
+        this(type, host, port, database, username, password, null);
+    }
+
+    public Database(DatabaseType type, String host, int port, String database, String username, String password, PoolSettings settings) {
+        if (type == DatabaseType.MARIADB_POOL) {
+            if (settings == null) {
+                this.poolSettings = new PoolSettings();
+            }
+            this.type = type;
+            this.host = host;
+            this.port = port;
+            this.database = database;
+            this.username = username;
+            this.password = password;
+        } else if (type == DatabaseType.MARIADB_CONNECTION) {
+            this.type = type;
+            this.host = host;
+            this.port = port;
+            this.database = database;
+            this.username = username;
+            this.password = password;
+            try {
+                this.connection = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database + "?autoreconnect=true", this.username, this.password);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void setupDataSource(PoolSettings settings) {
+        Properties properties = new Properties();
+        properties.setProperty("dataSourceClassName", "org.mariadb.jdbc.MariaDbDataSource");
+        properties.setProperty("dataSource.serverName", this.host);
+        properties.setProperty("dataSource.portNumber", this.port + "");
+        properties.setProperty("dataSource.user", this.username);
+        properties.setProperty("dataSource.password", this.password);
+        properties.setProperty("dataSource.databaseName", this.database);
+
+        HikariConfig config = this.poolSettings.getConfig(properties);
+        this.dataSource = new HikariDataSource(config);
+    }
+
+    public Connection getConnection() {
+        if (type == DatabaseType.MARIADB_POOL) {
+            if (dataSource == null) {
+                setupDataSource(poolSettings);
+            }
+            try {
+                return dataSource.getConnection();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (type == DatabaseType.MARIADB_CONNECTION) {
+            return this.connection;
+        }
+        return null;
+    }
+
+    public void closeConnection() {
+        if (type == DatabaseType.MARIADB_POOL) {
+            if (dataSource != null) {
+                dataSource.close();
+            }
+        } else if (type == DatabaseType.MARIADB_CONNECTION) {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public boolean isConnected() {
+        if (type == DatabaseType.MARIADB_POOL) {
+            if (dataSource != null) {
+                return dataSource.isRunning();
+            }
+        } else if (type == DatabaseType.MARIADB_CONNECTION) {
+            if (connection != null) {
+                try {
+                    return !connection.isClosed();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+
+    public DatabaseType getType() {
+        return type;
+    }
+
+    public void execute(String sql) {
+        try {
+            if (type == DatabaseType.MARIADB_CONNECTION) {
+                Connection connection = getConnection();
+                connection.createStatement().execute(sql);
+            } else if (type == DatabaseType.MARIADB_POOL) {
+                if (this.connection != null && !connection.isClosed()) {
+                    this.connection.createStatement().execute(sql);
+                } else {
+                    this.connection = getConnection();
+                    this.connection.createStatement().execute(sql);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet executeQuery(String sql) {
+        try {
+            if (type == DatabaseType.MARIADB_CONNECTION) {
+                Connection connection = getConnection();
+                return connection.createStatement().executeQuery(sql);
+            } else if (type == DatabaseType.MARIADB_POOL) {
+                if (this.connection != null && !connection.isClosed()) {
+                    return this.connection.createStatement().executeQuery(sql);
+                } else {
+                    this.connection = getConnection();
+                    return this.connection.createStatement().executeQuery(sql);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public PreparedStatement prepareStatement(String sql) {
+        try {
+            if (type == DatabaseType.MARIADB_CONNECTION) {
+                Connection connection = getConnection();
+                return connection.prepareStatement(sql);
+            } else if (type == DatabaseType.MARIADB_POOL) {
+                if (this.connection != null && !connection.isClosed()) {
+                    return this.connection.prepareStatement(sql);
+                } else {
+                    this.connection = getConnection();
+                    return this.connection.prepareStatement(sql);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getDatabaseName() {
+        return this.database;
+    }
+
+    public DBManager getDBManager() {
+        return new DBManager(this);
+    }
+}
